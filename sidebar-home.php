842acc595b1af5c58b77f2d0bf94dfe7b343fdf9
Jeremy Harris <jharris@rockharbor.org>
2011-09-15 12:13:33 -0700

    Styled different accordion titles

--- sidebar-home.php
+++ sidebar-home.php
@@ -7,18 +7,18 @@ $twitteruser = $theme->options('twitter_user');
 	<div class="body">
 		<p>Some facebook crap</p>
 	</div>
-	<div class="title"><h3>Twitter</h3><a class="title-icon" target="_blank" href="http://twitter.com/<?php echo $twitteruser; ?>">Visit Twitter</a></div>
+	<div class="title reverse"><h3>Twitter</h3><a class="title-icon" target="_blank" href="http://twitter.com/<?php echo $twitteruser; ?>">Visit Twitter</a></div>
 	<div class="body">
 		<?php 
 		$theme->set('term', $theme->options('twitter_search'));
 		echo $theme->render('twitter'); 
 		?>
 	</div>
-	<div class="title"><h3>Share Your Story</h3></div>
+	<div class="title white"><h3>Share Your Story</h3></div>
 	<div class="body">
 		<p>Some form crap</p>
 	</div>
-	<div class="title"><h3>Calendar</h3><a class="title-icon" target="_blank" href="http://core.rockharbor.org">Visit CORE</a></div>
+	<div class="title white reverse"><h3>Calendar</h3><a class="title-icon" target="_blank" href="http://core.rockharbor.org">Visit CORE</a></div>
 	<div class="body">
 		<p>Some calendar crap</p>
 	</div>
