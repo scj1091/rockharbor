15c3e12f8e706dff052b9632a141b8ab5fde785f
Jeremy Harris <jharris@rockharbor.org>
2011-09-20 11:31:16 -0700

    Tweaked styles and spacing

--- sidebar-core.php
+++ sidebar-core.php
@@ -2,13 +2,11 @@
 	<?php
 	global $post, $theme;
 	$metadata = $theme->metaToData($post->ID);
-	if (isset($metadata['core_id']) || isset($metadata['core_event_id'])) {
-		$meta = array_merge(array(
-			'core_id' => 0,
-			'core_event_id' => 0
-		), $metadata);
-		$theme->set('events', $theme->getCoreMinistryEvents($meta['core_id'], $meta['core_event_id']));
-		echo $theme->render('core_public_calendar');
-	}
+	$meta = array_merge(array(
+		'core_id' => 0,
+		'core_event_id' => 0
+	), $metadata);
+	$theme->set('events', $theme->getCoreMinistryEvents($meta['core_id'], $meta['core_event_id']));
+	echo $theme->render('core_public_calendar');
 	?>
 </div>
