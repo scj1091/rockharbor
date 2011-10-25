637f2b1366c9fda0ba57be12478649220cf08251
Jeremy Harris <jharris@rockharbor.org>
2011-10-25 15:47:02 -0700

    Changed behavior of submenu to always show the root most page's children, except if it has no root in which it will show its children

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,16 @@
 <?php global $post; ?>
 <div id="sub-navigation" class="widget-area">
 	<ul>
-	<?php 
+	<?php
+	$ancestors = get_post_ancestors($post->ID);
+	if (empty($ancestors)) {
+		$page = $post->ID;
+	} else {
+		$root = count($ancestors)-1;
+		$page = $ancestors[$root];
+	}
 	wp_list_pages(array(
-		'child_of' => $post->ID,
+		'child_of' => $page,
 		'title_li' => null
 	));
 	?>
