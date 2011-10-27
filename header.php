90247f4dc05c0b938ffffd6290e787717709cc57
Jeremy Harris <jharris@rockharbor.org>
2011-10-27 07:39:30 -0700

    Added breadcrumb

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php global $theme; ?>
+<?php global $post, $theme; ?>
 <?php get_template_part('header', 'prebody') ?>
 <body <?php body_class(); ?>>
 
@@ -18,6 +18,27 @@
 
 		<?php if (isset($_GET['message'])): ?>
 		<div class="message"><?php echo $_GET['message']; ?></div>
+		<?php endif; ?>
+		
+		<?php if (!empty($post->post_parent)): ?>
+		<nav class="breadcrumb">
+			<?php
+			$ancestors = get_post_ancestors($post->ID);
+			$ancestors = array_reverse($ancestors);
+			$links = array();
+			$sep = '&nbsp;<span class="secondary">&gt;</span>&nbsp;';
+			foreach ($ancestors as $ancestor) {
+				$ancestorPost = get_post($ancestor);
+				$links[] = $theme->Html->tag('a', $ancestorPost->post_title, array(
+					'href' => get_permalink($ancestorPost->ID),
+					'title' => esc_attr($ancestorPost->post_title)
+				));
+			}
+			$links[] = $post->post_title;
+			
+			echo implode($sep, $links);
+			?>
+		</nav>
 		<?php endif; ?>
 
 		<div id="main" class="clearfix">
