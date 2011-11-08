75982c55a692ef03dd1b8c5319f5db3b04b14f5d
Jeremy Harris <jharris@rockharbor.org>
2011-11-08 09:24:15 -0800

    Added support for multiple columns in dropdowns containing more than 5 elements. #YAWPH.

--- header.php
+++ header.php
@@ -12,10 +12,55 @@
 		<header id="branding" role="banner">
 			<div id="banner" class="clearfix"><?php echo $theme->render('banner'); ?></div>
 			<nav id="access" role="navigation" class="clearfix">
-				<?php wp_nav_menu(array('theme_location' => 'main', 'menu_class' => 'menu clearfix', 'fallback_cb' => function() { })); ?>
+				<?php
+				$locations = get_nav_menu_locations();
+				$menu = wp_get_nav_menu_object($locations['main']);
+				$menu_items = wp_get_nav_menu_items($menu->term_id);
+				_wp_menu_item_classes_by_context($menu_items);
+				$menu = array();
+				$ids = array();
+				foreach ($menu_items as $key => $menu_item) {
+					$a = $theme->Html->tag('a', $menu_item->title, array('href' => $menu_item->url));
+					$opts = array(
+						'class' => implode(' ', $menu_item->classes)
+					);
+					if ($menu_item->menu_item_parent == 0) {
+						// top level
+						$menu[] = array(
+							'a' => $a,
+							'opts' => $opts,
+							'children' => array()
+						);
+						$ids[$menu_item->ID] = count($menu)-1;
+					} else {
+						// child
+						$menu[$ids[$menu_item->menu_item_parent]]['children'][] = $theme->Html->tag('li', $a, $opts);
+					}
+				}
+				$output = '';
+				$max_row = 5;
+				foreach ($menu as $key => $top_level_menu_item) {
+					$children = null;
+					if (!empty($top_level_menu_item['children'])) {
+						$class = null;
+						if (count($top_level_menu_item['children']) > $max_row) {
+							$top_level_menu_item['children'] = array_chunk($top_level_menu_item['children'], $max_row);
+							foreach ($top_level_menu_item['children'] as $col) {
+								$children .= $theme->Html->tag('ul', implode('', $col));
+							}
+							$class = 'cols'.count($top_level_menu_item['children']);
+						} else {
+							$children = $theme->Html->tag('ul', implode('', $top_level_menu_item['children']));
+						}
+						$children = $theme->Html->tag('div', $children, array('class' => 'submenu '.$class));
+					}
+					$output .= $theme->Html->tag('li', $top_level_menu_item['a'].$children, $top_level_menu_item['opts']);
+				}
+				echo $theme->Html->tag('ul', $output, array('class' => 'menu clearfix'));
+				?>
 			</nav>
 		</header>
-
+		
 		<?php if (isset($_GET['message'])): ?>
 		<div class="message"><?php echo $_GET['message']; ?></div>
 		<?php endif; ?>
