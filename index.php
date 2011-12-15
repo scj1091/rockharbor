2513b846497110d6d0996f8c23dee12b7fe840d2
Jeremy Harris <jharris@rockharbor.org>
2011-12-14 17:04:57 -0800

    Removed archive template from base theme

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php 
-$archive = $theme->isArchive();
 get_header(); 
 ?>
 		<section id="content" role="main">
@@ -18,9 +17,6 @@ get_header();
 				while (have_posts()) {
 					the_post();
 					$sub = get_post_type();
-					if ($archive) {
-						$sub .= '-more';
-					}
 					get_template_part('content', $sub); 
 				}
 				$theme->set('wp_rewrite', $wp_rewrite);
