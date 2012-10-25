04d8379b1b5f770aed0b13c1ed872897b2622a44
Jeremy Harris <jharris@rockharbor.org>
2012-10-25 14:47:56 -0700

    Added 404 template

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php 
+get_header(); 
+?>
+		<section id="content" role="main">
+			<header id="content-title">
+				<h1 class="page-title">
+					<span>
+						<?php echo wp_title(''); ?>
+					</span>
+				</h1>
+			</header>
+
+			<article id="post-0" class="post no-results not-found">
+				<header class="entry-header clearfix">
+					<h1 class="entry-title"><?php _e('Nothing Found', 'rockharbor'); ?></h1>
+				</header>
+
+				<div class="entry-content">
+					<p><?php _e('Apologies, but this page or file does not exist!', 'rockharbor'); ?></p>
+					<?php get_search_form(); ?>
+				</div>
+			</article>
+
+		</section>
+
+<?php 
+get_footer();
