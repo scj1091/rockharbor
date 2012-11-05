a432bc912ee95897d640c62166858807f9000e16
Jeremy Harris <jharris@rockharbor.org>
2012-11-05 08:16:16 -0800

    Removed breadcrumb

--- header.php
+++ header.php
@@ -112,26 +112,5 @@
 			unset($_SESSION['message']);
 		}
 		?>
-		
-		<?php if (!empty($post->post_parent)): ?>
-		<nav class="breadcrumb">
-			<?php
-			$ancestors = get_post_ancestors($post->ID);
-			$ancestors = array_reverse($ancestors);
-			$links = array();
-			$sep = '&nbsp;<span class="secondary">&gt;</span>&nbsp;';
-			foreach ($ancestors as $ancestor) {
-				$ancestorPost = get_post($ancestor);
-				$links[] = $theme->Html->tag('a', $ancestorPost->post_title, array(
-					'href' => get_permalink($ancestorPost->ID),
-					'title' => esc_attr($ancestorPost->post_title)
-				));
-			}
-			$links[] = $post->post_title;
-			
-			echo implode($sep, $links);
-			?>
-		</nav>
-		<?php endif; ?>
 
 		<div id="main" class="clearfix">
