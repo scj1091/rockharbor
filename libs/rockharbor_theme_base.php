77ea5b62c44d55737e8fd30befb32e2621f691b2
Jeremy Harris <jharris@rockharbor.org>
2012-11-06 08:38:11 -0800

    Appended 'Widget' to widget class names

--- libs/rockharbor_theme_base.php
+++ libs/rockharbor_theme_base.php
@@ -616,10 +616,10 @@ class RockharborThemeBase {
 			'after_title' => '</h3>',
 		));
 		
-		include_once 'widgets' . DS . 'core.php';
-		register_widget('Core');
-		include_once 'widgets' . DS . 'social.php';
-		register_widget('Social');
+		include_once 'widgets' . DS . 'core_widget.php';
+		register_widget('CoreWidget');
+		include_once 'widgets' . DS . 'social_widget.php';
+		register_widget('SocialWidget');
 	}
 
 /**
