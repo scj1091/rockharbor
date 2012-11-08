7f96761a06d025f07368208716da5219e846fd8d
Jeremy Harris <jharris@rockharbor.org>
2012-11-08 12:11:33 -0800

    Styled content and asides on homepage

--- js/iefixes.js
+++ js/iefixes.js
@@ -8,5 +8,7 @@
 		$(".stories-2 .story-box:nth-child(2n+2)").addClass('nthreset');
 		$(".stories-3 .story-box:nth-child(3n+3)").addClass('nthreset');
 		$(".stories-4 .story-box:nth-child(4n+4)").addClass('nthreset');
+		$(".home #frontpage-sidebar aside:nth-child(odd)").addClass('nthreset');
+		$(".home #frontpage-sidebar aside:nth-child(even)").addClass('nthreset');
 	});
 }(jQuery))
