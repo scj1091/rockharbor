8ad80996307a18f631491a67c6e6172b6a06d21b
Jeremy Harris <jharris@rockharbor.org>
2012-11-12 14:12:31 -0800

    Combined mobile and touch js into touch.js

--- js/touch.js
+++ js/touch.js
@@ -1,6 +1,22 @@
 var RH = RH || {};
 
 /**
+ * Called when entering the tablet breakpoint
+ */
+RH.tabletEnter = function() {
+	// make nav the first element
+	jQuery('#branding nav').detach().prependTo('#branding');
+}
+
+/**
+ * Called when exiting the tablet breakpoint
+ */
+RH.tabletExit = function() {
+	// make nav the last element
+	jQuery('#branding nav').detach().appendTo('#branding');
+}
+
+/**
  * Called when entering the mobile breakpoint
  */
 RH.mobileEnter = function() {
