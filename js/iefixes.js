5f43c62b691544a9c05ec22c730904b38f89086a
Jeremy Harris <jharris@rockharbor.org>
2012-11-27 12:34:41 -0800

    IE inline-block and nth-child reset fixes

--- js/iefixes.js
+++ js/iefixes.js
@@ -5,9 +5,12 @@
 		// fix for lack of nth-child support
 		$('#access ul li div.submenu.cols2 ul:nth-child(2n+2)').addClass('nthreset');
 		$('#access ul li div.submenu.cols2 ul:nth-child(3n+3)').addClass('nthreset');
-		$(".stories-2 .story-box:nth-child(2n+2)").addClass('nthreset');
-		$(".stories-3 .story-box:nth-child(3n+3)").addClass('nthreset');
-		$(".stories-4 .story-box:nth-child(4n+4)").addClass('nthreset');
+		$(".stories-2 .story-box:nth-child(2n+2)").addClass('marginreset');
+		$(".stories-3 .story-box:nth-child(3n+3)").addClass('marginreset');
+		$(".stories-4 .story-box:nth-child(4n+4)").addClass('marginreset');
+		$(".stories-2 .story-box:nth-child(2n+3)").addClass('nthreset');
+		$(".stories-3 .story-box:nth-child(3n+4)").addClass('nthreset');
+		$(".stories-4 .story-box:nth-child(4n+5)").addClass('nthreset');
 		$(".home #frontpage-sidebar aside:nth-child(odd)").addClass('nthreset');
 		$(".home #frontpage-sidebar aside:nth-child(even)").addClass('nthreset');
 		$("#content .series-collection article.message-series:nth-child(4n+5)").addClass('nthreset');
