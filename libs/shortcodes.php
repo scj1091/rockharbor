2699ae5442345661affb225c7f1929b7b29ef582
Jeremy Harris <jharris@rockharbor.org>
2012-12-13 10:06:59 -0800

    Added address shortcode

--- libs/shortcodes.php
+++ libs/shortcodes.php
@@ -29,6 +29,21 @@ class Shortcodes {
 		add_shortcode('ebulletin-archive', array($this, 'ebulletinArchive'));
 		add_shortcode('children-grid', array($this, 'childrenGrid'));
 		add_shortcode('service-times', array($this, 'serviceTimes'));
+		add_shortcode('address', array($this, 'address'));
+	}
+	
+/**
+ * Renders address
+ * 
+ * @param array $attr Attributes sent by WordPress defined in the editor
+ * @return string
+ */
+	public function address($attr) {
+		$attrs = shortcode_atts(array(
+			'campus' => $this->theme->info('id')
+		), $attr);
+		$this->theme->set('address', $this->theme->options('address', false, $attrs['campus']));
+		return $this->theme->render('address');
 	}
 	
 /**
