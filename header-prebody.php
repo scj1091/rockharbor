46b82ff9467cdc96d155b547275593f64593d0e0
Jeremy Harris <jharris@rockharbor.org>
2013-03-26 14:05:28 -0700

    Removed unnecessary is_front_page call

--- header-prebody.php
+++ header-prebody.php
@@ -24,7 +24,7 @@
 
 	// Add the blog description for the home/front page.
 	$site_description = get_bloginfo('description', 'display');
-	if ($site_description && (is_front_page() || is_front_page())) {
+	if ($site_description && is_front_page()) {
 		echo " | $site_description";
 	}
 	?></title>
