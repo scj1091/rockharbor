c04b5a896b46e915f0ca3b468064714180f7b9e5
Jeremy Harris <jharris@rockharbor.org>
2013-05-09 11:38:39 -0700

    Removed duplicate link tag

--- header-prebody.php
+++ header-prebody.php
@@ -30,7 +30,6 @@
 	?></title>
 <link rel="icon" href="<?php echo $theme->info('url'); ?>/img/favicon.ico" type="image/x-icon" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
