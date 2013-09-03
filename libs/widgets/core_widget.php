09730da73ae5ea9711881c95d456f09387ac069b
Joseph Benson <joseph.benson@alumni.usc.edu>
2013-09-03 16:01:59 -0700

    Fixes conditional from 3bad7085b3

--- libs/widgets/core_widget.php
+++ libs/widgets/core_widget.php
@@ -38,7 +38,8 @@ class CoreWidget extends Widget {
 
 		if (empty($data['core_id']) && empty($data['core_involvement_id'])) {
 			$metadata = $this->theme->metaToData($post->ID);
-			if (empty($metadata['limit']) && !empty($data['limit']))
+			// If $metadata['limit'] not 0 or a positive integer ignore it
+			if (!($metadata['limit'] === 0 || $metadata['limit'] > 0))
 				$metadata['limit'] = $data['limit'];
 			$data = array_merge($data, $metadata);
 		}
