430e8b5af1f5576cb035801032c907758dddb8d0
Joseph Benson <joseph.benson@alumni.usc.edu>
2017-05-31 21:05:52 -0700

    Ads FB Pixel code to header

--- header-prebody.php
+++ header-prebody.php
@@ -37,7 +37,20 @@ if (is_front_page()) {
 <meta property="og:url" content="<?php echo $pageUrl; ?>" />
 <meta property="og:image" content="<?php echo $pageImage; ?>" />
 <meta name="description" content="<?php echo substr($postExcerpt, 0, 155); ?>" />
-
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '151499425391027'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=151499425391027&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 <link rel="icon" href="<?php echo $theme->Html->image('favicon.ico', array( 'parent' => true, 'url' => true )); ?>" type="image/x-icon" />
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $theme->info('url'); ?>/img/appicon-144.png" />
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $theme->info('url'); ?>/img/appicon-114.png" />
